eabff318325cb59945f4f1503871d101a9f31404
brunoLloret <163921771+brunoLloret@users.noreply.github.com>
2024-06-17 15:54:25 -0400

    Responsive site about to deploy mobile-dimensions with screen event listener

---
+++ website-project/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import NavBarButtons from "./components/NavBarButtons"
+import Header from "./components/Header"
+import MoreAboutMe from './components/MoreAbout'
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <title>Landing page</title>
+      </head>
+      <body>
+        <div className=" mx-auto bg-white rounded-xl overflow-hidden ">
+          <div className="">
+            <div className="">
+              <div className="flex flex-col items-center pb-10" >
+                <Header />
+                <h2 className="flex-center font-mono">Ongoing projects</h2>
+
+                <div>
+                  <NavBarButtons />
+                </div>
+                <br></br>
+
+                <MoreAboutMe />
+              </div >
+            </div>
+          </div>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App
